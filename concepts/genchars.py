ec52d578a98a8ba00a96b5b2707b483a219c8245
Home\Daniel <dankenji27@gmail.com>
2015-08-23 20:13:32 -0300

    Changes
    Destroyed a duplicate

---
+++ concepts/genchars.py
@@ -0,0 +1,17 @@
+﻿# Takes an integer "n" and a character, and returns a string of the character
+# that is "n" times long.
+
+def generate_n_chars(n, char):
+    count = 0
+    word = ""
+    templist = []
+    while count < n:
+        count += 1
+        templist.append(char)
+    word = "".join(templist)
+    return word
+
+if __name__ == "__main__":
+    n = int(input("Enter an integer: "))
+    char = input("Enter a character: ")
+    print(generate_n_chars(n, char))
---
+++ histogram.py
@@ -0,0 +1,14 @@
+﻿# Takes a list of integers and prints a histogram.
+# Random testing implemented.
+
+import random
+
+def histogram(intlist):
+    for number in intlist:
+        print(number * "*")
+    
+
+if __name__ == "__main__":
+    intlist = random.sample(range(1, 12), 3)
+    histogram(intlist)
+    
